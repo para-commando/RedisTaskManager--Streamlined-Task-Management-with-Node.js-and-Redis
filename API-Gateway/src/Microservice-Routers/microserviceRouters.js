449e6f7147059ac25837c1904c918a71d03d57a6
anirudh-nayak-172k <anirudh.nayak.172k@gmail.com>
2023-07-20 23:16:02 +0530

    reset-password: router level design

--- API-Gateway/src/Microservice-Routers/microserviceRouters.js
+++ API-Gateway/src/Microservice-Routers/microserviceRouters.js
@@ -1,12 +1,47 @@
 const app = require('../app');
 const {
-  registerUserMiddlewares, loginUserMiddlewares, sendOtpMiddlewares, verifyOtpMiddlewares
+  registerUserMiddlewares, loginUserMiddlewares, sendOtpMiddlewares, verifyOtpMiddlewares, resetPasswordMiddlewares
 } = require('../Middlewares/Route-Middlewares/expressRateLimit.middleware');
 const Joi = require('joi');
 const {
   authenticationProcesses,
 } = require('../../../sub-systems/Authentication-System/Processes/process');
 const logger = require('../../../shared/src/configurations/logger.configurations');
+app.post(
+  '/routes/Task-Management-system/SubSystem/user-Authentication/reset-Password',
+  resetPasswordMiddlewares.expressRateLimiterMiddleware,
+  async (req, res, next) => {
+    try {
+      const schema = Joi.object({
+        phoneNo: Joi.string().pattern(new RegExp('^[0-9]{10}$')).required(),
+        userName: Joi.string().min(3).max(30).required(),
+        password: Joi.string().min(8).max(30).required(),
+      });
+      const validatedData = schema.validate(req.body);
+      if (validatedData?.error) {
+        throw {
+          status: 400,
+          message: 'Bad Request',
+          error: validatedData?.error,
+        };
+      } else {
+        const { phoneNo, userName, password } = validatedData.value;
+        const response = await authenticationProcesses.changePassword({
+          userName: userName,
+          password: password,
+          phoneNo: phoneNo,
+        });
+        logger.info('🚀response: ', response);
+        res.status(200).json({
+          responseData: response,
+        });
+      }
+    } catch (error) {
+      logger.error('This is an error message.');
+      res.status(400).json({ error: error });
+    }
+  }
+);
 app.post(
   '/routes/Task-Management-system/SubSystem/user-Authentication/verify-OTP',
   verifyOtpMiddlewares.expressRateLimiterMiddleware,
