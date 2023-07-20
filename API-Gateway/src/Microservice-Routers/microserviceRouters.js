04b6bdb396f67672fdd257881abc2b04218ede3c
anirudh-nayak-172k <anirudh.nayak.172k@gmail.com>
2023-07-20 22:46:38 +0530

    send-otp: router level design

--- API-Gateway/src/Microservice-Routers/microserviceRouters.js
+++ API-Gateway/src/Microservice-Routers/microserviceRouters.js
@@ -1,12 +1,44 @@
 const app = require('../app');
 const {
-  registerUserMiddlewares,loginUserMiddlewares,
+  registerUserMiddlewares, loginUserMiddlewares, sendOtpMiddlewares
 } = require('../Middlewares/Route-Middlewares/expressRateLimit.middleware');
 const Joi = require('joi');
 const {
   authenticationProcesses,
 } = require('../../../sub-systems/Authentication-System/Processes/process');
 const logger = require('../../../shared/src/configurations/logger.configurations');
+
+app.post(
+  '/routes/Task-Management-system/SubSystem/user-Authentication/send-OTP',
+  sendOtpMiddlewares.expressRateLimiterMiddleware,
+  async (req, res, next) => {
+    try {
+      const schema = Joi.object({
+        phoneNo: Joi.string().pattern(new RegExp('^[0-9]{10}$')).required(),
+      });
+      const validatedData = schema.validate(req.body);
+      if (validatedData?.error) {
+        throw {
+          status: 400,
+          message: 'Bad Request',
+          error: validatedData?.error,
+        };
+      } else {
+        const { phoneNo } = validatedData.value;
+        const response = await authenticationProcesses.sendOtp({
+          phoneNo: phoneNo,
+        });
+        logger.info('🚀response: ', response);
+        res.status(200).json({
+          responseData: response,
+        });
+      }
+    } catch (error) {
+      logger.error('This is an error message.');
+      res.status(400).json({ error: error });
+    }
+  }
+);
 
 app.post(
   '/routes/Task-Management-system/SubSystem/user-Authentication/login-user',
